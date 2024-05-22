28dcd06ec844f3b3c579be5ce16ecd10d79172f5
LeeBumS <134660554+LeeBumS@users.noreply.github.com>
2024-05-22 17:44:54 +0900

    Add files via upload

---
+++ m01_ 데이터베이스/Task.sql
@@ -0,0 +1,204 @@
+
+
+-- 2024년 5월 17일
+
+-- TASK1_0517 출판사가 '굿스포츠' 혹은 '대한미디어' 인 도서를 검색하세요 (3가지 방법)
+SELECT * FROM BOOK WHERE (publisher = '굿스포츠') OR (publisher='대한미디어');
+
+SELECT * FROM BOOK WHERE publisher IN('굿스포츠','대한미디어');
+
+SELECT * FROM BOOK WHERE (publisher = '굿스포츠') UNION SELECT * FROM BOOK WHERE (publisher='대한미디어');
+
+-- TASK2_0517 출판사가 '굿스포츠' 혹은 '대한미디어' 가 아닌 도서를 검색
+SELECT * FROM BOOK WHERE publisher NOT IN('굿스포츠','대한미디어');
+
+-- TASK3_0517 축구에 관한 도서 중 가격이 20,000원 이상인 도서를 검색하세요
+SELECT bookname FROM BOOK WHERE bookname LIKE '%축구%' AND PRICE > 20000;
+
+--Task4_0517. 2번 김연아 고객이 주문한 도서의 총 판매액을 구하시오.
+SELECT customer.name, orders.custid, SUM(orders.saleprice) AS "총 판매액"
+FROM orders, customer WHERE orders.custid = 2 AND orders.custid=customer.custid
+GROUP BY customer.name, orders.custid;
+
+SELECT customer.name, orders.custid, SUM(orders.saleprice) AS "총 판매액"
+FROM orders INNER JOIN customer ON orders.custid = customer.custid
+WHERE orders.custid = 2 GROUP BY customer.name, orders.custid;
+
+--Task5_0517. 가격이 8,000원 이상인 도서를 구매한 고객에 대하여 고객별 주문 도서의 총 수량을 구하시오. 
+--단, 두 권 이상 구매한 고객만 구하시오.
+SELECT custid, COUNT(*) AS 도서수량 FROM orders WHERE saleprice >= 8000 
+GROUP BY custid HAVING COUNT(*) >=2; 
+
+--Task6_0517. 고객의 이름과 고객이 주문한 도서의 판매가격을 검색하시오.
+SELECT name, saleprice FROM customer, orders WHERE customer.custid = orders.custid;
+
+--Task7_0517. 고객별로 주문한 모든 도서의 총 판매액을 구하고, 고객별로 정렬하시오.
+SELECT custid, SUM(saleprice) "총 판매액" FROM orders GROUP BY custid
+ORDER BY custid;
+
+SELECT name, SUM(saleprice) AS "총 판매액" FROM customer C, orders O
+WHERE c.custid = o.custid GROUP BY c.name ORDER BY c.name;
+
+--------------------------------------------------------------------------------------------------------------------------------
+
+-- 2024년 5월 20일
+
+--Task1_0520. 10개의 속성으로 구성되는 테이블 2개를 작성하세요. 단 FOREIGN KEY를 적용하여 한쪽 테이블의 데이터를 삭제 시 
+--다른 테이블의 관련되는 데이터도 모두 삭제되도록 하세요. (모든 제약조건을 사용)
+--단, 각 테이블에 5개의 데이터를 입력하고 두번째 테이블에 첫번째 데이터를 참조하고 있는 속성을 선택하여 데이터 삭제 
+DROP table mart;
+DROP table department;
+create table mart(
+    custid number primary key
+    , name varchar2(20)
+    , age number
+    , sx varchar2(20)
+    , phone number not null
+    , address varchar2(100)
+    , frequency number -- 방문 빈도
+    , amount_num number
+    , amount_price number
+    , parking varchar2(20) -- 주차여부
+    , family number -- 가족 구성원 수
+);
+
+alter table mart drop column amount_num;
+alter table mart modify (name varchar2(30));
+alter table mart modify (phone varchar2(20));
+
+DESC mart;
+insert into mart values(1, '고길동', 32, '남', '010-1234-1234', '서울 강남', 5, 1500000, 'N', 3);
+insert into mart values(2, '손흥민', 31, '남', '010-7777-1234', '강원 춘천', 5, 200000000, 'Y', 4);
+insert into mart values(3, '이순신', 57, '남', '010-1592-1234', '경남 통영', 5, 270000, 'N', 1);
+insert into mart values(4, '아이유', 30, '여', '010-0516-1234', '서울 서초', 5, 750000000, 'Y', 4);
+insert into mart values(5, '임영웅', 30, '남', '010-0517-1235', '서울 역삼', 5, 75000000, 'Y', 2);
+
+select * from mart;
+
+create table department(
+    custid number primary key
+    , name varchar(20)
+    , age number
+    , sx varchar2(20)
+    , phone number not null
+    , address varchar2(100)
+    , use_store varchar2(100) -- 자주 찾는 매장
+    , amount_num number
+    , amount_price number
+    , valet varchar2(20) -- 발렛파킹 서비스 사용여부
+    , rounge varchar2(20) -- vip 라운지 사용여부
+    , foreign key (custid) references mart(custid) on delete cascade
+);
+
+alter table department modify (amount_price check (amount_price > 100000000));
+alter table department modify (rounge default 'Y');
+alter table department modify (valet default 'Y');
+alter table department modify (phone varchar2(100));
+alter table department drop column amount_num;
+--alter table department add (custid number);
+select * from department;
+
+insert into department values(1, '손흥민', 31, '남', '010-7777-1234', '강원 춘천', 'LV', 900000000,'','');
+insert into department values(2, '아이유', 30, '여', '010-0516-1234', '서울 서초', 'GUCCI', 1500000000,'','');
+insert into department values(3, '박지성', 31, '남', '010-7775-1235', '강원 춘천', 'LV', 900000000,'','');
+insert into department values(4, '박세리', 30, '여', '010-0516-1234', '서울 서초', 'GUCCI', 1500000000,'','');
+insert into department values(5, '임영웅', 30, '남', '010-0517-1235', '서울 역삼', 'ROLEX', 150000000,'','');
+
+DELETE mart
+WHERE custid = 1;
+
+--Task2_0520. Customer 테이블에서 박세리 고객의 주소를 김연아 고객의 주소로 변경하시오.
+UPDATE Customer SET address = 
+(SELECT address FROM Customer WHERE name = '김연아')
+WHERE name = '박세리';
+
+SELECT * FROM customer;
+
+-- 다시 부산으로 변견
+UPDATE customer SET ADDRESS = '대한민국 부산' WHERE NAME = '박세리';
+
+--Task3_0520. 도서 제목에 ‘야구’가 포함된 도서를 ‘농구’로 변경한 후 도서 목록, 가격을 보이시오.
+UPDATE Book SET bookname = REPLACE(bookname, '야구', '농구')
+WHERE bookname LIKE '%야구%';
+SELECT bookname, price
+FROM Book;
+
+SELECT BOOKID, REPLACE(BOOKNAME, '야구','농구') BOOKNAME, PUBLISHER,PRICE FROM BOOK;
+
+--Task4_0520. 마당서점의 고객 중에서 같은 성(姓)을 가진 사람이 몇 명이나 되는지 성별 인원수를 구하시오.
+-- SUBSTR(NAME,1,1) 함수는 문자열 NAME의 첫번째 글자부터 시작하여 한 글자를 반환
+-- GROUP BY 절에서는 별칭이 아닌 SUBSTR(NAME,1,1) 표현식을 사용해야 함
+SELECT SUBSTR(NAME,1,1) 성, COUNT(*) 인원 
+FROM CUSTOMER GROUP BY SUBSTR(NAME,1,1);
+
+--Task5_0520. 마당서점은 주문일로부터 10일 후 매출을 확정한다. 각 주문의 확정일자를 구하시오.
+SELECT orderid, orderdate, orderdate + 10 AS confirmation_date FROM Orders;
+
+SELECT ORDERID, ORDERDATE AS 주문일, ORDERDATE + 10 AS 확정일자 FROM ORDERS;
+
+--마당서점은 주문일로부터 2개월 후 매출을 확정한다. 각 주문의 확정일자를 구하시오.
+SELECT ORDERID, ORDERDATE AS 주문일, ADD_MONTHS(ORDERDATE, 2) AS 확정일자 FROM ORDERS;
+
+--Task6_0520. 마당서점이 2020년 7월 7일에 주문받은 도서의 주문번호, 주문일, 고객번호, 도서번호를 모두 보이시오. 
+--단 주문일은 ‘yyyy-mm-dd 요일’ 형태로 표시한다.
+SELECT ORDERID 주문번호, TO_CHAR(ORDERDATE, 'YYYY-MM-DD DAY') 주문일, CUSTID 고객번호, BOOKID 도서번호
+FROM ORDERS WHERE ORDERDATE = '2020-07-07';
+
+-- WHRER ORDERDATE = TO_DATE('20/07/07', 'YY/MM/DD')
+-- WHRER ORDERDATE = TO_DATE('20/07/07', 'DD/MM/YY')
+
+--Task7_0520. 평균 주문금액 이하의 주문에 대해서 주문번호와 금액을 보이시오.
+SELECT orderid, saleprice
+FROM Orders WHERE saleprice < (SELECT AVG(saleprice) FROM Orders);
+
+SELECT orderid, saleprice
+FROM Orders WHERE saleprice <= (SELECT AVG(saleprice)
+AS avg_amount FROM Orders);
+
+SELECT O1.ORDERID, O1.SALEPRICE FROM ORDERS O1 
+WHERE O1.SALEPRICE < (SELECT AVG(O2.SALEPRICE) FROM ORDERS O2); 
+
+-- 서브쿼리를 O2라는 별칭으로 지정, SALEPRCIE의 평균 값을 AVG_SALEPRICE로 계산
+SELECT O1.ORDERID, O1.SALEPRICE FROM ORDERS O1 JOIN (SELECT AVG(SALEPRICE) AS AVG_SALEPRICE
+FROM ORDERS) O2 ON O1.SALEPRICE < O2.AVG_SALEPRICE;
+
+--Task8_0520. 대한민국’에 거주하는 고객에게 판매한 도서의 총 판매액을 구하시오.
+SELECT SUM(saleprice) AS 총판매액 FROM ORDERS WHERE CUSTID IN (SELECT CUSTID 
+FROM CUSTOMER WHERE ADDRESS LIKE '%대한민국%');                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ m01_ 데이터베이스/실습2-2024_5-22.sql
@@ -0,0 +1,183 @@
+
+
+--[실습]
+-- 현재 HR에 있는 7개 테이블들을 분석해서 인사관리에 의미있는 인사이트 5개 이상을 기술하세요
+-- 인사관리 개선을 위한 KPI 2개를 설정하고 이것들을 테이블들의 데이터에 반영한 후 다시 분석해서 반영 여부를 검증
+
+SELECT * FROM EMPLOYEES;
+SELECT * FROM countries;
+SELECT * FROM departments;
+SELECT * FROM job_history;
+SELECT * FROM locations;
+SELECT * FROM regions;
+SELECT * FROM jobs;
+
+
+-- CASE1 : 성과금 계산
+SELECT EMPLOYEE_ID, LAST_NAME, ROUND(CASE WHEN SALARY > (SELECT AVG(SALARY) 
+FROM EMPLOYEES) THEN SALARY * 0.1 ELSE SALARY * 0.05 END) AS 성과금 FROM EMPLOYEES ORDER BY 성과금 DESC;
+
+-- CASE : 근무년수에 따른 보상
+SELECT 
+    LAST_NAME,
+    HIRE_DATE,
+    TRUNC(MONTHS_BETWEEN(SYSDATE, HIRE_DATE) / 12) AS 근무년수,
+    CASE
+        WHEN TRUNC(MONTHS_BETWEEN(SYSDATE, HIRE_DATE) / 12) >= 20 THEN '한달 포상휴가'
+        WHEN TRUNC(MONTHS_BETWEEN(SYSDATE, HIRE_DATE) / 12) >= 18 THEN '2주 포상휴가'
+        WHEN TRUNC(MONTHS_BETWEEN(SYSDATE, HIRE_DATE) / 12) >= 16 THEN '1주 포상휴가'
+        WHEN TRUNC(MONTHS_BETWEEN(SYSDATE, HIRE_DATE) / 12) >= 14 THEN '3일 포상휴가'
+        ELSE '휴가 없음'
+    END AS 휴가
+FROM EMPLOYEES ORDER BY 근무년수 DESC;
+
+-- CASE : 직급
+SELECT 
+    EMPLOYEE_ID AS 사원번호,
+    LAST_NAME AS 이름,
+    HIRE_DATE AS 입사일,
+    CASE
+        WHEN TRUNC(SYSDATE - HIRE_DATE) >= 8000 THEN '임원'
+        WHEN TRUNC(SYSDATE - HIRE_DATE) >= 7000 AND SALARY > (SELECT AVG(SALARY) FROM EMPLOYEES) THEN '본부장'
+        WHEN TRUNC(SYSDATE - HIRE_DATE) >= 7000 THEN '부장'
+        WHEN TRUNC(SYSDATE - HIRE_DATE) >= 5000 AND SALARY > (SELECT AVG(SALARY) FROM EMPLOYEES) THEN '차장'
+        WHEN TRUNC(SYSDATE - HIRE_DATE) >= 5000 THEN '과장'
+        WHEN TRUNC(SYSDATE - HIRE_DATE) >= 3000 AND SALARY > (SELECT AVG(SALARY) FROM EMPLOYEES) THEN '대리'
+        ELSE '사원'
+    END AS 직급
+FROM 
+    EMPLOYEES;
+    
+
+-- CASE: 직무 연장자
+SELECT 
+    e.EMPLOYEE_ID AS 사원번호,
+    e.LAST_NAME AS 이름,
+    e.JOB_ID,
+    e.SALARY AS 급여,
+    e.HIRE_DATE AS 입사일
+FROM EMPLOYEES e WHERE (e.JOB_ID, e.SALARY, e.HIRE_DATE) IN (SELECT JOB_ID,MAX(SALARY) AS MAX_SALARY,
+MIN(HIRE_DATE) AS MIN_HIRE_DATE FROM EMPLOYEES GROUP BY JOB_ID) ORDER BY 사원번호 DESC;
+
+-- CASE: 커미션 수준에서 가장 높은 보상을 받는 직원을 식별
+SELECT 
+    EMPLOYEE_ID AS 사원번호,
+    LAST_NAME AS 이름,
+    COMMISSION_PCT AS 커미션,
+    SALARY AS 급여
+FROM EMPLOYEES WHERE (COMMISSION_PCT, SALARY) IN (SELECT COMMISSION_PCT,
+MAX(SALARY) AS MAX_SALARY FROM EMPLOYEES GROUP BY 
+COMMISSION_PCT);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
