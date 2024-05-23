b3c9b6de28a37fb41d8c43e7acadc87056632b2a
LeeBumS <134660554+LeeBumS@users.noreply.github.com>
2024-05-23 17:40:10 +0900

    Add files via upload

--- m01_ 데이터베이스/Task.sql
+++ m01_ 데이터베이스/Task.sql
@@ -167,12 +167,37 @@ SELECT SUM(saleprice) AS 총판매액 FROM ORDERS WHERE CUSTID IN (SELECT CUSTID
 FROM CUSTOMER WHERE ADDRESS LIKE '%대한민국%');                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
 
 
+-- 2024년 5월 23일
+--Task1_0523. 
+--orders라는 테이블을 생성하고, order_id, customer_id, order_date, amount, status라는 속성을 설정하세요.
+--데이터를 5개 입력하세요.
+--입력한 데이터 중 2개를 수정하세요.
+--수정한 데이터를 취소하기 위해 롤백을 사용하세요.
+--3개의 새로운 데이터를 입력하고, 그 중 마지막 데이터 입력만 취소한 후 나머지를 커밋하세요.
+DROP TABLE ORDERS;
+
+CREATE TABLE orders (
+order_id NUMBER PRIMARY KEY,
+customer_id VARCHAR2(100),
+order_date DATE,
+amount VARCHAR2(100),
+STATUS VARCHAR2(20));
+
+INSERT INTO orders (order_id, customer_id, order_date, amount, status) VALUES (1, '유재석',  SYSDATE, '대',  'Active');
+INSERT INTO orders (order_id, customer_id, order_date, amount, status) VALUES (2, '김종국',  SYSDATE, '중','Active');
+INSERT INTO orders (order_id, customer_id, order_date, amount, status) VALUES (3, '양세찬',  SYSDATE, '소', 'Inactive');
 
 
+SELECT * FROM orders;
 
+SAVEPOINT TASK1;
 
+INSERT INTO orders (order_id, customer_id, order_date, amount, status) VALUES (4, '송지효', SYSDATE, '대',  'Active');
+INSERT INTO orders (order_id, customer_id, order_date, amount, status) VALUES (5, '지석진', SYSDATE, '소', 'Inactive');
 
+ROLLBACK TO TASK1;
 
+COMMIT;
 
 
 
